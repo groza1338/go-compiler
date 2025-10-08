5f24d37d0806d7f327c957ba9532db3c69994246
Senkalia <ereminfrims2004@gmail.com>
2025-10-08 19:15:37 +0300

    feat: Добавил тест для raw строки.

---
+++ tests/strings/raw_str.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    raw1 := `Hello, world!`
+
+    raw2 := `This is a raw string
+with a newline inside.`
+
+    raw3 := `A string with a carriage return \r but it will be ignored.`
+
+    raw4 := `This is the first line.
+This is the second line,
+and it includes more text!`
+
+    raw5 := ``
+}
