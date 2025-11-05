d4821282207cf332fda66f76bb19b7e2a27d5914
Senkalia <ereminfrims2004@gmail.com>
2025-11-06 00:59:16 +0300

    feat: Создал класс ExprNode.

--- classes.cpp
+++ classes.cpp
@@ -5,3 +5,241 @@
 #include "classes.h"
 
 unsigned int AstNode::maxId = 0;
+
+ExprNode* ExprNode::createIdentifier(const string &value) {
+    ExprNode *node = new ExprNode();
+    node->type = ID;
+    node->identifier = value;
+    return node;
+}
+
+ExprNode* ExprNode::createIntLiteral(int value) {
+    ExprNode *node = new ExprNode();
+    node->type = INT_LITERAL;
+    node->intLiteral = value;
+    return node;
+}
+
+ExprNode* ExprNode::createFloatLiteral(float value) {
+    ExprNode *node = new ExprNode();
+    node->type = FLOAT_LITERAL;
+    node->floatLiteral = value;
+    return node;
+}
+
+ExprNode* ExprNode::createRuneLiteral(int value) {
+    ExprNode *node = new ExprNode();
+    node->type = RUNE_LITERAL;
+    node->runeLiteral = value;
+    return node;
+}
+
+ExprNode* ExprNode::createStringLiteral(const string &value) {
+    ExprNode *node = new ExprNode();
+    node->type = STRING_LITERAL;
+    node->stringLiteral = value;
+    return node;
+}
+
+ExprNode* ExprNode::createBoolLiteral(bool value) {
+    ExprNode *node = new ExprNode();
+    node->type = BOOL_LITERAL;
+    node->boolLiteral = value;
+    return node;
+}
+
+ExprNode* ExprNode::createSummary(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = SUMMARY;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createSubtraction(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = SUBTRACTION;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createMultiplication(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = MULTIPLICATION;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createDivision(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = DIVISION;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createEqual(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = EQUAL;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createNotEqual(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = NOT_EQUAL;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createLess(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = LESS;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createGreater(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = GREATER;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createLessOrEqual(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = LESS_OR_EQUAL;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createGreaterOrEqual(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = GREAT_OR_EQUAL;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createAnd(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = AND;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createOr(ExprNode *left, ExprNode *right) {
+    ExprNode *node = new ExprNode();
+    node->type = OR;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+ExprNode* ExprNode::createNot(ExprNode *operand) {
+    ExprNode *node = new ExprNode();
+    node->type = NOT_EQUAL;
+    node->left = operand;
+    return node;
+}
+
+ExprNode* ExprNode::createUnaryMinus(ExprNode *operand) {
+    ExprNode *node = new ExprNode();
+    node->type = UNARY_MINUS;
+    node->operand = operand;
+    return node;
+}
+
+ExprNode* ExprNode::createElementAccess(ExprNode *operand, ExprNode *index) {
+    ExprNode *node = new ExprNode();
+    node->type = ELEMENT_ACCESS;
+    node->operand = operand;
+    node->index = index;
+    return node;
+}
+
+ExprNode* ExprNode::createSlice(ExprNode *operand, ExprNode *low, ExprNode *high, ExprNode *max) {
+    ExprNode *node = new ExprNode();
+    node->type = SLICE;
+    node->operand = operand;
+    node->sliceLow = low;
+    node->sliceHigh = high;
+    node->sliceMax = max;
+    return node;
+}
+
+ExprNode* ExprNode::createFunctionCall(ExprNode *operand, list<ExprNode *> &args) {
+    ExprNode *node = new ExprNode();
+    node->type = FUNCTION_CALL;
+    node->operand = operand;
+    node->args = args;
+    return node;
+}
+
+ExprNode::Type ExprNode::getType() const {
+    return type;
+}
+
+string ExprNode::getIdentifier() const {
+    return identifier;
+}
+
+int ExprNode::getIntLiteral() const {
+    return intLiteral;
+}
+
+float ExprNode::getFloatLiteral() const {
+    return floatLiteral;
+}
+
+int ExprNode::getRuneLiteral() const {
+    return runeLiteral;
+}
+
+string ExprNode::getStringLiteral() const {
+    return stringLiteral;
+}
+
+bool ExprNode::getBoolLiteral() const {
+    return boolLiteral;
+}
+
+ExprNode* ExprNode::getLeft() const {
+    return left;
+}
+
+ExprNode* ExprNode::getRight() const {
+    return right;
+}
+
+ExprNode* ExprNode::getOperand() const {
+    return operand;
+}
+
+ExprNode* ExprNode::getIndex() const {
+    return index;
+}
+
+list<ExprNode*> ExprNode::getArgs() const {
+    return args;
+}
+
+ExprNode* ExprNode::getLow() const {
+    return sliceLow;
+}
+
+ExprNode* ExprNode::getHigh() const {
+    return sliceHigh;
+}
+
+ExprNode* ExprNode::getMax() const {
+    return sliceMax;
+}
--- classes.h
+++ classes.h
@@ -1,9 +1,13 @@
 //
 // Created by Silvitio on 05.11.2025.
 //
+#pragma once
 
-#ifndef GOLANG_COMPILER_CLASSES_H
-#define GOLANG_COMPILER_CLASSES_H
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
 
 class AstNode {
 protected:
@@ -14,4 +18,107 @@ public:
     AstNode() {id = ++maxId;};
 };
 
-#endif //GOLANG_COMPILER_CLASSES_H
+class ExprNode : public AstNode {
+public:
+    enum Type {
+        NONE,
+        ID,
+        INT_LITERAL,
+        FLOAT_LITERAL,
+        RUNE_LITERAL,
+        STRING_LITERAL,
+        BOOL_LITERAL,
+        SUMMARY,
+        SUBTRACTION,
+        MULTIPLICATION,
+        DIVISION,
+        EQUAL,
+        NOT_EQUAL,
+        LESS,
+        GREATER,
+        LESS_OR_EQUAL,
+        GREAT_OR_EQUAL,
+        AND,
+        OR,
+        NOT,
+        UNARY_MINUS,
+        ELEMENT_ACCESS,
+        SLICE,
+        FUNCTION_CALL
+    };
+
+    static ExprNode* createIdentifier(const string &value);
+    static ExprNode* createIntLiteral(int value);
+    static ExprNode* createFloatLiteral(float value);
+    static ExprNode* createRuneLiteral(int value);
+    static ExprNode* createStringLiteral(const string &value);
+    static ExprNode* createBoolLiteral(bool value);
+    static ExprNode* createSummary(ExprNode* left, ExprNode* right);
+    static ExprNode* createSubtraction(ExprNode* left, ExprNode* right);
+    static ExprNode* createMultiplication(ExprNode* left, ExprNode* right);
+    static ExprNode* createDivision(ExprNode* left, ExprNode* right);
+    static ExprNode* createEqual(ExprNode* left, ExprNode* right);
+    static ExprNode* createNotEqual(ExprNode* left, ExprNode* right);
+    static ExprNode* createLess(ExprNode* left, ExprNode* right);
+    static ExprNode* createGreater(ExprNode* left, ExprNode* right);
+    static ExprNode* createLessOrEqual(ExprNode* left, ExprNode* right);
+    static ExprNode* createGreaterOrEqual(ExprNode* left, ExprNode* right);
+    static ExprNode* createAnd(ExprNode* left, ExprNode* right);
+    static ExprNode* createOr(ExprNode* left, ExprNode* right);
+    static ExprNode* createNot(ExprNode* operand);
+    static ExprNode* createUnaryMinus(ExprNode* operand);
+    static ExprNode* createElementAccess(ExprNode* operand, ExprNode* index);
+    static ExprNode* createSlice(ExprNode* operand, ExprNode* low, ExprNode* high, ExprNode* max);
+    static ExprNode* createFunctionCall(ExprNode* operand, list<ExprNode*> &args);
+
+    Type getType() const;
+    string getIdentifier() const;
+    int getIntLiteral() const;
+    float getFloatLiteral() const;
+    int getRuneLiteral() const;
+    string getStringLiteral() const;
+    bool getBoolLiteral() const;
+    ExprNode* getLeft() const;
+    ExprNode* getRight() const;
+    ExprNode* getOperand() const;
+    ExprNode* getIndex() const;
+    list<ExprNode*> getArgs() const;
+    ExprNode* getLow() const;
+    ExprNode* getHigh() const;
+    ExprNode* getMax() const;
+
+protected:
+    Type type;
+    string identifier;
+    int intLiteral;
+    float floatLiteral;
+    int runeLiteral;
+    string stringLiteral;
+    bool boolLiteral;
+
+    ExprNode *left;
+    ExprNode *right;
+    ExprNode *operand;
+    ExprNode *index;
+    list<ExprNode*> args;
+    ExprNode *sliceLow;
+    ExprNode *sliceHigh;
+    ExprNode *sliceMax;
+
+    ExprNode(): AstNode() {
+        type = NONE;
+        identifier = "";
+        intLiteral = 0;
+        floatLiteral = 0;
+        runeLiteral = 0;
+        stringLiteral = "";
+        boolLiteral = false;
+        left = nullptr;
+        right = nullptr;
+        operand = nullptr;
+        index = nullptr;
+        sliceLow = nullptr;
+        sliceHigh = nullptr;
+        sliceMax = nullptr;
+    };
+};
