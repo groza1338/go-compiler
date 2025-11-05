ceaf8930f2abf3424622a484bbfa418860690aa2
Senkalia <ereminfrims2004@gmail.com>
2025-11-05 21:55:09 +0300

    feat: Добавил базовый класс узла дерева.

--- classes.h
+++ classes.h
@@ -5,4 +5,13 @@
 #ifndef GOLANG_COMPILER_CLASSES_H
 #define GOLANG_COMPILER_CLASSES_H
 
+class AstNode {
+protected:
+    static unsigned int maxId;
+    unsigned int id;
+
+public:
+    AstNode() {id = ++maxId;};
+};
+
 #endif //GOLANG_COMPILER_CLASSES_H
