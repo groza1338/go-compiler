67f3cb11faf117a65c163970c92a2307cf1e3414
Sergey Gvozdkov <groza_1337@mail.ru>
2025-12-05 00:10:17 +0300

    добавил тесты на iota

---
+++ tests/constants/iota_basic.go
@@ -0,0 +1,13 @@
+package main
+
+const (
+	first = iota
+	second
+	third = iota
+)
+
+func main() {
+	_ = first
+	_ = second
+	_ = third
+}
---
+++ tests/constants/iota_reset.go
@@ -0,0 +1,18 @@
+package main
+
+const (
+	a = iota
+	b
+)
+
+const (
+	c = iota
+	d
+)
+
+func main() {
+	_ = a
+	_ = b
+	_ = c
+	_ = d
+}
