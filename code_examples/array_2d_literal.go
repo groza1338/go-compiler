ffbf412f4b44e5d85430cfa87ecab39436fa9391
Senkalia <ereminfrims2004@gmail.com>
2026-01-19 21:55:15 +0300

    Доработал тест.

--- code_examples/array_2d_literal.go
+++ code_examples/array_2d_literal.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    arr := [2][3]int{[3]int{1, 2, 3}, [3]int{4, 5, 6}}
-    fmt.Print(arr[0][1])
-    fmt.Print(" ")
-    fmt.Print(arr[1][2])
+	arr := [2][3]int{[3]int{1, 2, 3}, [3]int{4, 5, 6}}
+	arr[0][2] = 33
+	fmt.Print(arr)
+	fmt.Print("\n")
+	fmt.Print(arr[0])
+	fmt.Print("\n")
+	fmt.Print(arr[1])
+	fmt.Print("\n")
+	fmt.Print(arr[1][2])
 }
