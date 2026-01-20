8c13d9a9a6f5b5d3f8a678aaf3032aae4528d897
Sergey Gvozdkov <groza_1337@mail.ru>
2026-01-20 22:54:23 +0300

    обновил input_switch

--- main_code_examples/input_switch.go
+++ main_code_examples/input_switch.go
@@ -8,6 +8,8 @@ func main() {
     fmt.Scan(&n)
 
     switch n {
+        case 10, 20, 30:
+            fmt.Print("ten or twenty or thirty")
         case -1:
             fmt.Print("minus one")
         case 0:
@@ -15,6 +17,13 @@ func main() {
         case 1:
             fmt.Print("one")
         default:
-            fmt.Print("other")
+            fmt.Print("other ")
+    }
+
+    switch {
+        case n > 100:
+            fmt.Print("> 100")
+        default:
+            fmt.Print("абоба")
     }
 }
